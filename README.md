63a73893ac75af27198805ec1632e7bdfab511c8
perrette <mahe.perrette@pik-potsdam.de>
2016-01-16 14:04:43 +0100

    added versioneer to track version number

--- README.md
+++ README.md
@@ -1,3 +1,6 @@
 simpik
 ======
 Infrastructure to run model simulations and explore parameter space.
+
+See SICOPOLIS-REMBO job file for an example of how to use simpik to 
+create a job file.
--- setup.py
+++ setup.py
@@ -1,16 +1,16 @@
 # import os, sys, re
 from distutils.core import setup
-# import versioneer
+import versioneer
 
 
-# with open('README.md') as file:
-#     long_description = file.read()
+with open('README.md') as file:
+    long_description = file.read()
 
 
 # Actually important part
 setup(name='simpik',
-      # version=versioneer.get_version(),
-      # cmdclass=versioneer.get_cmdclass(),
+      version=versioneer.get_version(),
+      cmdclass=versioneer.get_cmdclass(),
       author='Mahe Perrette',
       author_email='mahe.perrette@pik-potsdam.de',
       description='job submission for SICOPOLIS-REMBO',
@@ -20,8 +20,8 @@ setup(name='simpik',
       packages = ['simpik'],
       # package_data = {'nml2f90':['templates/*.f90', 'libraries/*f90']},
       # scripts = ['scripts/nml2f90', 'scripts/f2nml'],
-      # long_description=long_description,
-      # url='https://github.com/perrette/nml-to-f90',
+      long_description=long_description,
+      url='https://gitlab.com/greenrise/simpik',
       # license = "MIT",
       )
 
