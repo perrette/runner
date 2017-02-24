1cef046629021ff580e97c2f26fe1ec44d751d62
perrette <mahe.perrette@pik-potsdam.de>
2017-02-24 20:43:31 +0100

    remove job config I/O options (#14)

--- runner/job/__main__.py
+++ runner/job/__main__.py
@@ -7,9 +7,7 @@ from importlib import import_module
 import argparse
 import logging
 from runner.job import register
-from .config import json_config, load_config, __version__
-
-
+from runner import __version__
 
 
 # pull main job together
@@ -21,16 +19,6 @@ def main(argv=None):
                                   formatter_class=argparse.RawDescriptionHelpFormatter)
 
     job.add_argument('-v','--version', action='version', version=__version__)
-    job.add_argument('-c','--config-file', 
-                        help='load defaults from configuration file')
-    x = job.add_mutually_exclusive_group()
-    x.add_argument('-s','--saveas', 
-                   help='save selected defaults to config file and exit')
-    x.add_argument('-u', '--update-config', action="store_true", 
-                        help='-uc FILE is an alias for -c FILE -s FILE')
-    job.add_argument('--show', action="store_true", help='show config and exit')
-    job.add_argument('--full', action='store_false', dest='diff',
-                   help='save/show full config, not only differences from default')
     job.add_argument('--debug', action="store_true", help='print full traceback')
     
     top = argparse.ArgumentParser(parents=[job], add_help=False)
@@ -70,31 +58,8 @@ def main(argv=None):
     cmdargs = argv[i+1:]
     o = top.parse_args(topargs)  # no subcommands
 
-    # parse again with updated defaults
-    defaults = register._defaults[o.cmd].copy()  # --module
-
-    # read config file?
-    if o.config_file:
-        js = load_config(o.config_file, parser)
-        defaults.update(js)
-        
-    parser.set_defaults(**defaults)
-
     # now subparser 
     cmdo = parser.parse_args(cmdargs)
-
-    if o.update_config:
-        o.saveas = o.config_file
-
-    # save to file?
-    if o.saveas or o.show:
-        string = json_config(cmdo.__dict__, parser, diff=o.diff)
-        if o.saveas:
-            with open(o.saveas, 'w') as f:
-                f.write(string)
-        if o.show:
-            print(string)
-        return
 
     try:
         func(cmdo)
